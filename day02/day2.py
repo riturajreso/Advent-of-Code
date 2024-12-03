e4d0f3e0aa3fd5290b785571a838b1b2b415c219
Rituraj <riturajreso@gmail.com>
2024-12-03 19:20:07 +0530

    Update day2.py

--- day02/day2.py
+++ day02/day2.py
@@ -1,14 +1,26 @@
 def safe(lst):
-    return sorted(lst) in [lst, lst[::-1]] \
-       and all(1 <= abs(a-b) <= 3 for a, b in zip(lst, lst[1:]))
+    
+    is_ascending = all(lst[i] <= lst[i+1] for i in range(len(lst)-1))
+    is_descending = all(lst[i] >= lst[i+1] for i in range(len(lst)-1))
+    if not (is_ascending or is_descending):
+        return False
 
-part1_safe, other_safe = 0, 0
+    for i in range(len(lst)-1):
+        if not (1 <= abs(lst[i] - lst[i+1]) <= 3):
+            return False
+    return True
+
+safe_before_removal, safe_after_removal = 0, 0
 for line in open('input.txt'):
     L = [int(x) for x in line.split()]
     if safe(L):
-        part1_safe += 1
-    elif any(safe(L[:i] + L[i+1:]) for i in range(len(L))):
-        other_safe += 1
+        safe_before_removal += 1
+    else:
+        for i in range(len(lst)):
+            modified_lst = lst[:i] + lst[i+1:]
+            if is_safe_arrangement(modified_lst):
+                safe_after_removal += 1
+                break
 
-print('Part 1 safe:', part1_safe)
-print('Part 2 safe:', part1_safe + other_safe)
+print('Part 1 safe:', safe_before_removal)
+print('Part 2 safe:', safe_before_removal + safe_after_removal)
